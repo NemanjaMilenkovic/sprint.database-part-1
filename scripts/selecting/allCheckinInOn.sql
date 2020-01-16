ac0f4a73b6b22a548bd0c7ebea69766a4975a9a3
ryukikikie <rkieeeekk@gmail.com>
2020-01-16 10:46:23 +0900

    added addCheckinInOn

--- scripts/selecting/allCheckinInOn.sql
+++ scripts/selecting/allCheckinInOn.sql
@@ -1,1 +1,5 @@
---  your SQL here
+SELECT students.*, checkins.checked_in_at
+FROM students
+  INNER JOIN checkins
+  ON students.id = checkins.id
+WHERE EXTRACT (year FROM checked_in_at) = 2016 AND EXTRACT (month FROM checked_in_at) = 6;
